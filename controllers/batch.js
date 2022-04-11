905820d19b7a32f4a0262dffec133da6361b7644
Anish Roy <6275anishroy@gmail.com>
2022-04-11 23:51:42 +0530

    batch name in details

--- controllers/batch.js
+++ controllers/batch.js
@@ -1,5 +1,6 @@
 const Batch = require("../models/batch");
-const Lecture = require("../models/lecture")
+const Lecture = require("../models/lecture");
+const Program = require("../models/program");
 
 exports.add = async (req, res) => {
     const { shortName, longName, programId, divisions } = req.body;
@@ -13,7 +14,7 @@ exports.add = async (req, res) => {
 
     batch.save(async (err) => { // remove the param (to review)
         if (err) {
-            console.log(err);
+            // console.log(err);
             return res.status(400).json({
                 err: "NOT able to save subject in DB"
             });
@@ -68,22 +69,25 @@ exports.getDivision = async (req, res) => {
 exports.getBatchDetails = async (req, res) => {
     try {
         const { batchId } = req.query;
+        const batch = await Batch.findById(batchId);
+        const program = await Program.findById(batch.programId);
+        let batchName = program.shortName + ' ' + batch.shortName;
         const lectures = await Lecture.find({ batchId: batchId });
         let staffNumbers = lectures.map(lecture => {
             return lecture.staffId.toString()
         })
         let distinctStaff = new Set(staffNumbers)
-        totStaff = distinctStaff.size
+        totStaff = distinctStaff.size;
 
         let subjectNumbers = lectures.map(lecture => {
             return lecture.staffId.toString()
         })
         let distinctSubjects = new Set(subjectNumbers)
-        totSubs = distinctSubjects.size
+        totSubs = distinctSubjects.size;
 
-        return res.status(200).json({ totalTeachers: totStaff, totalSubjects: totSubs });
+        return res.status(200).json({ name: batchName, totalTeachers: totStaff, totalSubjects: totSubs });
     } catch (e) {
-        console.log(e)
+        // console.log(e)
         return res.status(500);
     }
 }
