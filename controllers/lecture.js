4814a52e80a890ae705f10cd5185adc8b9bf6360
Mahima <mahimabiswas28@gmail.com>
2022-04-14 14:47:36 +0530

    overlapping lectures bug fixed

--- controllers/lecture.js
+++ controllers/lecture.js
@@ -5,7 +5,15 @@ const Staff = require("../models/staff");
 
 exports.add = async (req, res) => {
 
-    const { staffId, subjectId, division, day, date, time, batchId } = req.body;
+    const {
+        staffId,
+        subjectId,
+        division,
+        day,
+        date,
+        time,
+        batchId
+    } = req.body;
 
     const lecture = new Lecture({
 
@@ -18,18 +26,49 @@ exports.add = async (req, res) => {
         batchId
     });
 
+    if (time['from'] > time['to'] || time['from'] === time['to']) {
+        return res.status(400).json({
+            err: "Lecture timing invalid"
+        });
+    }
+    if (time['from'] < 0000 || time['from'] > 2400 || time['to'] < 0000 || time['to'] > 2400) {
+        return res.status(400).json({
+            err: "Lecture timing invalid"
+        });
+    }
 
-    const lectures = await Lecture.find({ $and: [{ day : day }, { batchId: batchId }] })
-    for(i=0;i<lectures.length;i++)
-    {
-        if(time['from']>lectures[i].time['from'] && time['from']<lectures[i].time['to'] || time['to']>lectures[i].time['from'] && time['to']<lectures[i].time['to'])
-        {
-            return res.status(400).json({
-                err: "NOT able to save lecture in DB"
-            });
+
+
+    const lectures = await Lecture.find({
+        $and: [{
+            division: division
+        }, {
+            batchId: batchId
+        }]
+    });
+
+
+    for (let i = 0; i < lectures.length; i++) {
+
+        let isDaySame = false;
+
+        for (let j = 0; j < lectures[i].day.length; j++) {
+            if (day.includes(lectures[i].day[j])) {
+                isDaySame = true;
+            }
+        }
+
+        if (isDaySame) {
+            if ((lectures[i].time.to > time.from && lectures[i].time.from < time.to) ||
+                (lectures[i].time.to === time.to && lectures[i].time.from === time.from)) {
+                return res.status(400).json({
+                    err: "Lecture timing overlapping"
+                });
+            }
         }
     }
-    
+
+
     lecture.save(async (err) => { // remove the param (to review)
         if (err) {
             return res.status(400).json({
@@ -45,19 +84,46 @@ exports.add = async (req, res) => {
 
 exports._delete = async (req, res) => {
     try {
-        const { id } = req.body;
+        const {
+            id
+        } = req.body;
         const lectures = await Lecture.findByIdAndDelete(id);
-        return res.status(200).json({ lectures });
+        return res.status(200).json({
+            lectures
+        });
     } catch (e) {
         return res.status(500);
     }
 }
 exports.update = async (req, res) => {
     try {
-        const { id, staffId, subjectId, division, day, date, time, batchId } = req.body;
-        const lectures = await Lecture.findByIdAndUpdate(id, { $set: { staffId, subjectId, division, day, date, time, batchId } }, { new: true });
+        const {
+            id,
+            staffId,
+            subjectId,
+            division,
+            day,
+            date,
+            time,
+            batchId
+        } = req.body;
+        const lectures = await Lecture.findByIdAndUpdate(id, {
+            $set: {
+                staffId,
+                subjectId,
+                division,
+                day,
+                date,
+                time,
+                batchId
+            }
+        }, {
+            new: true
+        });
 
-        return res.status(200).json({ lectures });
+        return res.status(200).json({
+            lectures
+        });
     } catch (e) {
         return res.status(500);
     }
@@ -70,22 +136,39 @@ exports.update = async (req, res) => {
 exports.get = async (req, res) => {
     let result = []
     try {
-        const { batchId, division } = req.query;
+        const {
+            batchId,
+            division
+        } = req.query;
 
-        const lectures = await Lecture.find({ $and: [{ batchId: batchId }, { division: division }] })
+        const lectures = await Lecture.find({
+            $and: [{
+                batchId: batchId
+            }, {
+                division: division
+            }]
+        })
         for (i = 0; i < lectures.length; i++) {
             const subId = lectures[i].subjectId.toString();
             let staffId = lectures[i].staffId.toString();
             let subjectDetails = await Subject.findById(subId)
             let staffDetails = await Staff.findById(staffId)
-            result[i] = { name: staffDetails.name, shortName: subjectDetails.shortName, longName: subjectDetails.longName, type: subjectDetails.type, time: lectures[i].time, day: lectures[i].day, date: lectures[i].date }
+            result[i] = {
+                name: staffDetails.name,
+                shortName: subjectDetails.shortName,
+                longName: subjectDetails.longName,
+                type: subjectDetails.type,
+                time: lectures[i].time,
+                day: lectures[i].day,
+                date: lectures[i].date
+            }
             console.log(result[i])
         }
-        return res.status(200).json({ result: result });
-    }
-    catch (e) {
+        return res.status(200).json({
+            result: result
+        });
+    } catch (e) {
         console.log(e)
         return res.status(500);
     }
 };
-
